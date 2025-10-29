970af30ec4cf294ba63d66794a1905721d00266f
Zhukov Kirill <zhukovk4010@gmail.com>
2025-10-29 20:10:39 +0300

    add api get ingredients

--- src/app/constructor/constructor.ts
+++ src/app/constructor/constructor.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ConstructorStore} from './data/constructor.store';
 
 @Component({
   selector: 'app-constructor',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './constructor.html',
   styleUrl: './constructor.scss'
 })
-export class Constructor {
+export class Constructor implements OnInit {
+  protected readonly constructorStore = inject(ConstructorStore);
 
+  public ngOnInit(): void {
+    this.constructorStore.getIngredients();
+  }
 }
--- src/app/constructor/data/constructor.store.ts
+++ src/app/constructor/data/constructor.store.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {IngredientsApi} from './ingredients.api';
 
 @Injectable({
     providedIn: 'root'
   })
 export class ConstructorStore {
+  private readonly ingredientsApiService = inject(IngredientsApi);
 
+  public getIngredients() {
+    this.ingredientsApiService.getIngredients().subscribe({
+      next: (res) => {
+        console.log(res)
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
 }
